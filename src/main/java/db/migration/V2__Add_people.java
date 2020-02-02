c796be8773dc471026798bd69c8d18f20289bf08
Shaun Lefeuvre <slefeuvre@mtab.com>
2020-02-01 17:36:15 -0800

    Database and migrations.

---
+++ src/main/java/db/migration/V2__Add_people.java
@@ -0,0 +1,14 @@
+package db.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+public class V2__Add_people extends BaseJavaMigration {
+    @Override
+    public void migrate(Context context) throws Exception {
+        new JdbcTemplate(new SingleConnectionDataSource(context.getConnection(), true))
+                .execute("INSERT INTO person (id, name) VALUES (0, 'Shaun')");
+    }
+}
--- src/main/java/net/remiohead/website/HelloController.java
+++ src/main/java/net/remiohead/website/HelloController.java
@@ -1,14 +1,36 @@
 package net.remiohead.website;
 
+import net.remiohead.website.models.Tables;
+import net.remiohead.website.models.tables.Person;
+import net.remiohead.website.models.tables.records.PersonRecord;
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.requireNonNull;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
+    private final DSLContext context;
+
+    @Autowired
+    public HelloController(final DSLContext context) {
+        this.context = requireNonNull(context);
+    }
+
     @RequestMapping()
-    public String index() {
-        return "Hello World";
+    public List<String> index() {
+        return this.context
+                .selectFrom(Tables.PERSON)
+                .fetch()
+                .stream()
+                .map(PersonRecord::getName)
+                .collect(Collectors.toList());
     }
 }
