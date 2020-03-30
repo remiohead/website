3c4435b94395a2ce34484a52d12ff6e4a524dca1
Shaun Lefeuvre <slefeuvre@mtab.com>
2020-03-29 17:29:14 -0700

    Added date stamp.

--- src/main/java/net/remiohead/website/weather/WeatherResponse.java
+++ src/main/java/net/remiohead/website/weather/WeatherResponse.java
@@ -1,5 +1,8 @@
 package net.remiohead.website.weather;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -14,8 +17,14 @@ public class WeatherResponse implements Supplier<String> {
         final Map<String, Object> result =
                 new WeatherTask("98106,us").get();
 
+        final LocalDateTime date = LocalDateTime
+                .now(ZoneId.of("America/Los_Angeles"));
+        final DateTimeFormatter formatter = DateTimeFormatter
+                .ofPattern("yyyy-MM-dd HH:mm:ss");
+
         final StringBuilder out = new StringBuilder();
-        out.append("The temperature is ");
+        out.append("At ").append(formatter.format(date)).append(" ");
+        out.append("the temperature is ");
         out.append(result.get("temp"));
         out.append(" degrees with ");
         out.append(result.get("humidity"));
