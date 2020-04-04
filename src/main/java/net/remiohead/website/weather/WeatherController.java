8b9668a4632c78ea9ed615519e4eaeb51a16281b
Shaun Lefeuvre <slefeuvre@mtab.com>
2020-04-04 10:24:59 -0700

    Fixed not receiving messages.

--- src/main/java/net/remiohead/website/weather/WeatherController.java
+++ src/main/java/net/remiohead/website/weather/WeatherController.java
@@ -1,26 +1,33 @@
 package net.remiohead.website.weather;
 
-import net.remiohead.website.weather.WeatherConsumer;
-import org.apache.kafka.clients.consumer.Consumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import static java.util.Objects.requireNonNull;
 
 @RestController
 public class WeatherController {
 
     private final WeatherConsumer consumer;
+    private final WeatherProducer producer;
 
     @Autowired
     public WeatherController(
-            final WeatherConsumer consumer) {
-        this.consumer = consumer;
+            final WeatherConsumer consumer,
+            final WeatherProducer producer) {
+        this.consumer = requireNonNull(consumer);
+        this.producer = requireNonNull(producer);
     }
 
     @GetMapping("/weather")
-    public String get() {
+    public String weather() {
         return this.consumer.getMessage();
     }
+
+    @GetMapping("/trigger")
+    public void trigger() {
+        this.producer.post();
+    }
+
 }
